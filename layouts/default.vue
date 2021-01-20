6bf22c6daf4ff670a29eea2f431e7647f5d996c4
yuma <yuma_shimomura@safarigames.com>
2021-01-20 17:52:38 +0900

    ページ実装
    関連コンテンツ実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,54 +1,34 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+  <v-app>
+    <v-app-bar
       fixed
       app
     >
-      <v-list>
-        <v-list-item
+      <v-tabs>
+        <v-tab
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
           router
           exact
+          class="tab-items"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
+          <v-icon>{{ item.icon }}</v-icon>
+          <span>{{item.title}}</span>
+        </v-tab>
+      </v-tabs>
+
+<!--        <v-hover-->
+<!--          v-for="(item, i) in items"-->
+<!--          :key="i"-->
+<!--          :to="item.to"-->
+<!--          router-->
+<!--          exact>-->
+<!--          <v-card class="d-flex flex-row pa-2" outlined tile>{{item.title}}</v-card>-->
+<!--        </v-hover>-->
     </v-app-bar>
     <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-main>
     <v-footer
       :absolute="!fixed"
@@ -63,8 +43,6 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
       items: [
         {
@@ -78,11 +56,19 @@ export default {
           to: '/develop'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
+  },
+  methods: {
   }
 }
 </script>
+
+<style scoped>
+/*.tab-items {*/
+/*  color: #111;*/
+/*}*/
+/*.tab-items:hover {*/
+/*  background-color: #ffa07a;*/
+/*  color: #fff;*/
+/*}*/
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,117 @@
 <template>
-<v-container>
-  <v-btn
-    @click="$router.push('/develop')"
-  >
-    開発コンポーネント
-  </v-btn>
-</v-container>
+<v-app style="background-color: #ffe">
+  <!-- トップ大きい画像 -->
+  <imageLayer />
+  <!--
+  関連コンテンツ
+    youtube
+    booth
+    ホロライブ公式プロフィール
+    twitter
+  -->
+  <v-container class="center">
+    <v-item-group>
+      <v-row
+        v-for="(items, i) in getContentList()"
+        :key="i"
+        class="mb-6"
+      >
+        <v-col
+          class="d-flex justify-space-around">
+          <v-sheet
+            v-for="(item, j) in items"
+            :key="j"
+            class="rounded-lg"
+            color="white"
+            elevation="4"
+            height="240"
+            width="240"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card>
+                <v-img
+                  :src="item.img.length ? item.img : '/polka/blankIcon.png'"
+                >
+                  <v-card-text
+                    v-if="hover && item.img.length"
+                    style="text-align: center;color: #000;background-color: gray;opacity: 0.8;height: 240px;"
+                    v-on:click="urlOpen(item.src)"
+                  >
+                    <div class="font-weight-light title mb-2">
+                      {{item.img.length ? item.text : ''}}
+                    </div>
+                  </v-card-text>
+                </v-img>
+              </v-card>
+            </v-hover>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-item-group>
+  </v-container>
+</v-app>
 </template>
 
 <script>
-
+import imageLayer from '~/components/imageLayer'
 export default {
+  components: {
+    imageLayer
+  },
+  data () {
+    return {
+      contentList: [
+        {
+          text: 'youtubeへのリンク',
+          img: '/polka/faceIcon.png',
+          src: 'https://www.youtube.com/channel/UCK9V2B22uJYu3N7eR_BT9QA'
+        },
+        {
+          text: 'twitterへのリンク',
+          img: '/polka/twitterIcon.png',
+          src: 'https://twitter.com/omarupolka'
+        },
+        {
+          text: '公式へのリンク',
+          img: '',
+          src:''
+        },
+        {
+          text: 'boothへのリンク',
+          img: '/polka/boothIcon.png',
+          src: 'https://hololive.booth.pm/'
+        }
+      ]
+    }
+  },
+  methods: {
+    getContentList () {
+      let i = 0, ret = [];
+      // 3つずつ取り出す
+      while (1) {
+        const tmp = this.contentList.slice(i, i + 3)
+        i += 3
+        ret.push(tmp)
+        if (tmp.length < 3) break
+      }
+      // 最後の要素が3つに足りなかったらblankで穴埋め
+      while (ret[ret.length-1].length < 3) {
+        ret[ret.length-1].push({text: '', img:''})
+      }
+      return ret
+    },
+    urlOpen (url) {
+      if (url.length > 0) {
+        window.open(url, '_blank')
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.center {
+  width: 60%;
+  margin: 0 auto;
+}
 </style>
