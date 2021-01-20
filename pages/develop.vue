cc83ef4543c824c274774be59ee79af8d3a7b615
yuma <yuma_shimomura@safarigames.com>
2021-01-20 12:16:41 +0900

    ページ実装
    不要ファイルを削除
    コンポーネント検証用ページを作成

---
+++ pages/develop.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <div class="text-center">
+          <topHeader />
+        </div>
+      </v-col>
+    </v-row>
+    <v-container class="space200">
+    </v-container>
+    <scroll-item />
+    <v-container class="space200">
+    </v-container>
+    <scale />
+    <v-container class="footer">
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import ScrollItem from '~/components/develop/scrollItem.vue'
+import topHeader from '~/components/develop/topHeader.vue'
+import Scale from '~/components/develop/scale.vue'
+
+export default {
+  components: {
+    topHeader,
+    ScrollItem,
+    Scale
+  }
+}
+</script>
+
+<style scoped>
+.space200 {
+  height: 200px;
+}
+.footer {
+  height: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,19 @@
 <template>
 <v-container>
-  <v-row justify="center" align="center">
-    <v-col cols="12">
-      <div class="text-center">
-        <TopHeader />
-      </div>
-    </v-col>
-  </v-row>
-  <v-container class="space200">
-  </v-container>
-  <scroll-item />
-  <v-container class="space200">
-  </v-container>
-  <scale />
-  <v-container class="footer">
-  </v-container>
+  <v-btn
+    @click="$router.push('/develop')"
+  >
+    開発コンポーネント
+  </v-btn>
 </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-import { mapActions } from 'vuex'
-import ScrollItem from '~/components/scrollItem.vue'
-import Scale from '~/components/scale.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-    ScrollItem,
-    Scale
-  }
 }
 </script>
 
 <style scoped>
-.space200 {
-  height: 200px;
-}
-.space480 {
-  height: 480px;
-}
-.footer {
-  height: 20px;
-}
+
 </style>
