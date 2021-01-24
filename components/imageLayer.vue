4f1ad44f9370b9f0e087ea4cee903b8183ce8b47
yuma346west <ub.yuma.cloe@gmail.com>
2021-01-25 00:54:27 +0900

    コンフリクト解消

---
+++ components/imageLayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="content">
+    <v-img
+      src="/polka/header3D.jfif"
+    >
+    </v-img>
+    <v-img
+      class="layered"
+      src="/polka/rect37.png"></v-img>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.content {
+  position: relative;
+}
+.layered {
+  position: absolute;
+  width: 20%;
+  top: 50%;
+  left: 50%;
+  transform : translate(-50%,-50%);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,84 +1,35 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+  <v-app>
+    <v-app-bar
       fixed
       app
     >
-      <v-list>
-        <v-list-item
+      <v-tabs>
+        <v-tab
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
           router
           exact
+          class="tab-items"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+          <v-icon>{{ item.icon }}</v-icon>
+          <span>{{item.title}}</span>
+        </v-tab>
+      </v-tabs>
+
+<!--        <v-hover-->
+<!--          v-for="(item, i) in items"-->
+<!--          :key="i"-->
+<!--          :to="item.to"-->
+<!--          router-->
+<!--          exact>-->
+<!--          <v-card class="d-flex flex-row pa-2" outlined tile>{{item.title}}</v-card>-->
+<!--        </v-hover>-->
     </v-app-bar>
     <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -92,26 +43,32 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-alert-outline',
           title: 'Welcome',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: '開発中',
+          to: '/develop'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
+  },
+  methods: {
   }
 }
 </script>
+
+<style scoped>
+/*.tab-items {*/
+/*  color: #111;*/
+/*}*/
+/*.tab-items:hover {*/
+/*  background-color: #ffa07a;*/
+/*  color: #fff;*/
+/*}*/
+</style>
